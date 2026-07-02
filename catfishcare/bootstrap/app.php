8548fbc989b2e6a483b887fdefbfcbfed62770d4
Zelectra <2341720243@student.polinema.ac.id>
2026-07-02 17:25:58 +0700

    fix: configure TrustProxies and APP_URL for secure HTTPS hosting on Vercel

--- catfishcare/bootstrap/app.php
+++ catfishcare/bootstrap/app.php
@@ -22,6 +22,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/login',
             'api/logout',
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
