1c22bdc823631796702467b81bc4ef9c505c2356
Zelectra <2341720243@student.polinema.ac.id>
2026-09-14 14:53:32 +0700

    fix(storage): bind ->useStoragePath('/tmp/storage') for Vercel serverless environment

--- catfishcare/bootstrap/app.php
+++ catfishcare/bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
@@ -42,3 +42,9 @@ return Application::configure(basePath: dirname(__DIR__))
             }
         });
     })->create();
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || (file_exists('/tmp') && !file_exists('c:\\'))) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
